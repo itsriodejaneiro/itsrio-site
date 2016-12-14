6d620ba9a4abb3e6a3459c71e12067bb3a5816e9
Ariadne Melo <ariadnevmelo@gmail.com>
2016-12-14 10:47:50 -0200

    título de conteúdos relacionados no plural, refs #73

--- wp-content/themes/its-rio/functions/components/related_content/view_related_content.php
+++ wp-content/themes/its-rio/functions/components/related_content/view_related_content.php
@@ -1,7 +1,7 @@
 <div class="related-content content-area" id="tab_<?= array_search('relacionados', $data['its_tabs']) ?>">
 	<div class="row">
 		<h2 class="list-title">
-			conteúdo relacionado
+			conteúdos relacionados
 			<div class="line"></div>
 		</h2>
 		<div class="related-post">
