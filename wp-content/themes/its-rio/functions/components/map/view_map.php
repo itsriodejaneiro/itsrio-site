8e96a175162e4c70d620946572ea9c135672c5e3
Ariadne Melo <ariadnevmelo@gmail.com>
2016-12-19 11:21:59 -0200

    layout do box de eventos do mapa, refs #110

--- wp-content/themes/its-rio/functions/components/map/view_map.php
+++ wp-content/themes/its-rio/functions/components/map/view_map.php
@@ -23,10 +23,18 @@
 				<div class="map-info-carousel-item carousel-cell" v-for="(info, i) in selectedMarker.infos" v-bind:style="{ 'background-image' : 'url('+ info.image +')' }">
 					<div class="close" @click="selectedMarker = false">&times;</div>
 					<div class="map-info-content">
-						<img v-if="info.image != ''" 
-						v-bind:src="info.image" alt="">
-						<h3>{{ info.title }}</h3> 	
-						<div v-html="info.text"></div>
+						<div class="map-thumb">
+							<div class="color-hover"></div><!--  -->
+							<!-- <img v-if="info.image != ''" 
+							v-bind:src="info.image" alt=""> -->
+							<img src="http://localhost/wp-content/uploads/2016/07/site2-direitos-autorais-na-pratica-05-1.png" alt="">
+						</div>
+						<div class="map-text">
+							<h3>Convenção XYZ Tecnologia (Recife, PE)</h3>
+							<div>Estudou Economia e Administração na Fundação Getulio Vargas do Rio de Janeiro. Possui cursos de Inovação, Criptografia, Psicologia e Filosofia da Mente pela Universidade de Cambridge, Reino Unido. Atuando especialmente nas frentes de Bitcoin, tecnologia blockchain, segurança digital e educação online, é pesquisador do ITS Rio.</div>
+							<!-- <h3 class="raleway">{{ info.title }}</h3> 	
+							<div v-html="info.text"></div> -->
+						</div>
 					</div>
 				</div>
 			</div>
