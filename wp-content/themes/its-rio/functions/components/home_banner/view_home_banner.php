f30c810a8f9b62073c2822f2d8612d631d8e04ec
Ariadne Melo <ariadnevmelo@gmail.com>
2017-01-04 17:09:16 -0200

    link no carousel apenas em dispositivos moveis

--- wp-content/themes/its-rio/functions/components/home_banner/view_home_banner.php
+++ wp-content/themes/its-rio/functions/components/home_banner/view_home_banner.php
@@ -15,7 +15,7 @@
 						<div class="categories-wrapper show-for-medium">
 							<?php $cat_classes = 'black'; include(ROOT. 'inc/categories.php') ?>
 						</div>
-						<div class="more-info">
+						<div class="more-info show-for-small-only">
 							<a href="<?= get_permalink() ?>">mais sobre a publicação</a>
 						</div>
 					</div>
