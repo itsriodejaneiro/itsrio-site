25445ef428783e6d2db471cf68d8a542663a1a97
Henrique Marques <henrique1_3@hotmail.com>
2017-01-27 16:53:45 -0200

    .

--- wp-content/themes/its-rio/inc/archive/info-curso-varanda.php
+++ wp-content/themes/its-rio/inc/archive/info-curso-varanda.php
@@ -11,7 +11,7 @@
 <div class="info-left">
 	<h2><?= the_title(); ?></h2>
 	<div class="show-for-medium">
-		<?php include(ROOT.'inc/palestrantes.php'); if(!is_null($posts)) $posts->reset_postdata(); ?>
+		<?php //include(ROOT.'inc/palestrantes.php'); if(!is_null($posts)) $posts->reset_postdata(); ?>
 	</div>
 </div>
 <div class="info-right horario show-for-medium">
