b96f368a75beeff917a06736cc6bb25198f00b05
Henrique Marques <henrique1_3@hotmail.com>
2017-01-27 18:31:38 -0200

    Retirar CTP do código

--- wp-content/themes/its-rio/assets/js/its.js
+++ wp-content/themes/its-rio/assets/js/its.js
@@ -49,7 +49,7 @@ Vue.component('its-search', {
     watch: {
         ctp: function ctp() {
             var _this = this;
-            if (this.ctp.indexOf('projetos_ctp') > -1 || this.ctp.indexOf('publicacoes_ctp') > -1) $('#info_areapesquisa').removeClass('hide');else $('#info_areapesquisa').addClass('hide');
+            if (this.ctp.indexOf('projetos') > -1 || this.ctp.indexOf('publicacoes') > -1) $('#info_areapesquisa').removeClass('hide');else $('#info_areapesquisa').addClass('hide');
 
             $('#cat-filter').customScrollbar({ skin: "default-skin" });
         }
