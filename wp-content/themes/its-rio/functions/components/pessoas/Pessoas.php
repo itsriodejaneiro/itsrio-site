046e77b1cd20dc7457c9e094ce122c4c5682e332
Henrique Marques <henrique1_3@hotmail.com>
2017-01-27 18:33:56 -0200

    Ordem dos professores no módulo divi

--- wp-content/themes/its-rio/functions/components/pessoas/Pessoas.php
+++ wp-content/themes/its-rio/functions/components/pessoas/Pessoas.php
@@ -56,7 +56,7 @@ class Pessoas extends ET_Builder_Module {
 		$ids = $meta['its_pessoas'];
 		$cats = [];
 		$listaCategorizada = false;
-		$query_palestrantes = get_posts(['post_type' => 'pessoas', 'include' => implode(',', $ids) ]);
+		$query_palestrantes = get_posts(['post_type' => 'pessoas', 'include' => implode(',', $ids) ], 'order' => 'ASC');
 		foreach ($query_palestrantes as $postt) {
 			$p = (array)$postt;
 			$cat = get_the_category($p['ID']);
