9c4f2b4cf0ddfa62031350272cb2b2b4f9b4e756
Henrique Marques <henrique1_3@hotmail.com>
2017-01-27 17:12:09 -0200

    .

--- wp-content/themes/its-rio/inc/archive/info-projeto-publi.php
+++ wp-content/themes/its-rio/inc/archive/info-projeto-publi.php
@@ -5,7 +5,7 @@
 <div class="info-left">
 	<h2><?= the_title(); ?></h2>
 	<div class="show-for-medium">
-		<?php include(ROOT.'inc/palestrantes.php'); if(!is_null($posts)) $posts->reset_postdata(); ?>
+		<?php include(ROOT.'inc/palestrantes.php'); if(isset($posts) && !is_null($posts)) $posts->reset_postdata(); ?>
 	</div>
 </div>
 <div class="info-right horario show-for-medium">
--- wp-content/themes/its-rio/inc/palestrantes.php
+++ wp-content/themes/its-rio/inc/palestrantes.php
@@ -5,12 +5,14 @@
 			<?php
 			$ids = $meta['its_pessoas'];
 			$html = '';
-			$query_pessoas = get_posts(['post_type' => 'pessoas', 'post__in' => $ids ]);
+			$query_pessoas = new WP_Query(['post_type' => 'pessoas', 'post__in' => $ids ]);
 			$i = 0;
-			foreach ($query_pessoas as $post){
+			while ($query_pessoas->have_posts()){
+				$query_pessoas->the_post();
 				$html .= ' <span onclick="goToPerson('.$i.')"> <a href="javascript:void(0);">'. get_the_title() . '</a></span>,';
 				$i++;
 			}
+			if(isset($query) && !is_null($query)) $query->reset_postdata();
 			echo rtrim($html,',');
 			?>
 		</b>
