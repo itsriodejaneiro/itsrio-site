de554588afb028bacc82c4c35e8aec6c643d9604
Henrique Marques <henrique1_3@hotmail.com>
2017-02-01 12:11:01 -0200

    .

--- wp-content/themes/its-rio/footer.php
+++ wp-content/themes/its-rio/footer.php
@@ -165,6 +165,7 @@
 		$(document).click(function() {
 			$('.single-header-drop-down').removeClass('active');
 		});
+		$('#media-player iframe').attr('src','https://www.youtube.com/embed/xcky7JQH-iY');
 
 		// $('.mejs-player').mediaelementplayer();
 		// setTimeout(function(){
