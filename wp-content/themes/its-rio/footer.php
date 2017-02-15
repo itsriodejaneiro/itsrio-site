bb466c385a6d7ab116f54000fd7eb60af41da5d3
Ariadne Melo <ariadnevmelo@gmail.com>
2017-02-15 16:01:19 -0200

    ajusta modal da newsletter, refs #266

--- wp-content/themes/its-rio/footer.php
+++ wp-content/themes/its-rio/footer.php
@@ -131,7 +131,16 @@
 
 	function openNewsletter(){
 		$('.reveal-overlay').fadeIn();
-		$('#modalNewsletter iframe').attr('src','http://itsrio.us12.list-manage2.com/subscribe?u=b2433258ec47fc6f9a063fd7b&id=8a308c4e7a&MERGE0='+$('#txtNewsletter').val());
+		$('#modalNewsletter iframe').attr('src','https://itsrio.us12.list-manage.com/subscribe/post?u=b2433258ec47fc6f9a063fd7b&id=8a308c4e7a&MERGE0='+$('#txtNewsletter').val());
+		setTimeout(function(){
+			if($('.reveal-overlay').is(':visible')){
+				$('html,body').css('position','relative');
+				$('html,body').css('overflow-y','hidden');
+			}else{
+				$('html,body').css('overflow-y','');
+				$('html,body').css('position','');
+			}
+		},500);
 	}
 
 	function changeMidia(obj, url, title){
