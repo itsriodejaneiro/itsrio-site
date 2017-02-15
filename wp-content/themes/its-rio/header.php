3b8b906ae63686c2972a6590f0b79dacfc57a99a
Henrique Marques <henrique1_3@hotmail.com>
2017-02-15 13:31:02 -0200

    Instant Articles e

--- wp-content/themes/its-rio/header.php
+++ wp-content/themes/its-rio/header.php
@@ -24,29 +24,56 @@
 	<script src="/wp-content/themes/its-rio/assets/js/isotope.pkgd.min.js"></script>
 	<script src="/wp-content/themes/its-rio/assets/js/jquery.custom-scrollbar.min.js"></script>
 	<script src="https://use.fontawesome.com/cb38949964.js"></script>
-</head>
-<body <?php body_class(); ?>>
-	<div id="content_all" v-cloak>
-		<?php
-		global $postType;
-		global $titles;
-		global $lang;
+	<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+			n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+				document,'script','https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '903842739667609'); 
+			fbq('track', 'PageView');
+		</script>
+		<noscript>
+			<img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=903842739667609&ev=PageView&noscript=1"/>
+		</noscript>
+		<!-- DO NOT MODIFY -->
+		<!-- End Facebook Pixel Code -->
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '1336747553030986'); // Insert your pixel ID here.
+		fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=1336747553030986&ev=PageView&noscript=1"
+		/></noscript>
+	</head>
+	<body <?php body_class(); ?>>
+		<div id="content_all" v-cloak>
+			<?php
+			global $postType;
+			global $titles;
+			global $lang;
 
-		$postType = get_post_type() ? get_post_type() : $wp_query->query['post_type'];
+			$postType = get_post_type() ? get_post_type() : $wp_query->query['post_type'];
 
-		if($lang == 'pt'){
-			$titles = [
-			'cursos_ctp' => 	['plural' => pll__('cursos'), 'singular' => pll__('curso')],
-			'varandas_ctp' => 	['plural' => pll__('varandas'), 'singular' => pll__('varanda')],
-			'projetos_ctp' => 	['plural' => pll__('projetos'), 'singular' => pll__('projeto')],
-			'publicacoes_ctp' =>['plural' => pll__('publicações'), 'singular' => pll__('publicação')],
-			'comunicados_ctp' =>['plural' => pll__('acontece'), 'singular' => pll__('acontece')],
-			'page' =>['plural' => pll__('institucionais'), 'singular' => pll__('institucional')]
-			];
-		}
+			if($lang == 'pt'){
+				$titles = [
+				'cursos_ctp' => 	['plural' => pll__('cursos'), 'singular' => pll__('curso')],
+				'varandas_ctp' => 	['plural' => pll__('varandas'), 'singular' => pll__('varanda')],
+				'projetos_ctp' => 	['plural' => pll__('projetos'), 'singular' => pll__('projeto')],
+				'publicacoes_ctp' =>['plural' => pll__('publicações'), 'singular' => pll__('publicação')],
+				'comunicados_ctp' =>['plural' => pll__('acontece'), 'singular' => pll__('acontece')],
+				'page' =>['plural' => pll__('institucionais'), 'singular' => pll__('institucional')]
+				];
+			}
 
-		global $title;
-		$title = $titles[$postType];
+			global $title;
+			$title = $titles[$postType];
 
 
 	/*if(is_front_page()){
