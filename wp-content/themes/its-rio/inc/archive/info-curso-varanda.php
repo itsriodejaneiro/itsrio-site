8b98bb3aada870d63fa465833d853e0c31d8f64f
Ariadne Melo <ariadnevmelo@gmail.com>
2017-02-17 11:26:23 -0200

    ajuste nas info de curso e varandas, refs #278

--- wp-content/themes/its-rio/inc/archive/info-curso-varanda.php
+++ wp-content/themes/its-rio/inc/archive/info-curso-varanda.php
@@ -5,7 +5,7 @@
 			echo date('d/m',strtotime($meta['info_data'][0])).' às '.$meta['info_hora'][0];
 		} 
 		if($postType == 'cursos_ctp'){
-			echo  pll__('inscrições até') . date('d/m',strtotime($meta['info_inscfim'][0])). "| ".pll__('início do curso')." " . date('d/m',strtotime($meta['info_data'][0]));
+			echo  pll__('inscrições até') . date('d/m',strtotime($meta['info_inscfim'][0])). " | ".pll__('início do curso')." " . date('d/m',strtotime($meta['info_data'][0]));
 		}
 		?>
 	</div>
@@ -24,7 +24,7 @@
 			echo date('d/m',strtotime($meta['info_data'][0])).' às '.$meta['info_hora'][0];
 		} 
 		if($postType == 'cursos_ctp'){
-			echo  pll__('inscrições até') .' '. date('d/m',strtotime($meta['info_inscfim'][0])). "| ".pll__('início do curso')." " . date('d/m',strtotime($meta['info_data'][0]));
+			echo  pll__('inscrições até') .' '. date('d/m',strtotime($meta['info_inscfim'][0])). " | ".pll__('início do curso')." " . date('d/m',strtotime($meta['info_data'][0]));
 		}
 		?>
 	</span>
