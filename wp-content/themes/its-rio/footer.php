61d71fd95f421279d4959d4d80c1f591fbc6b6f0
Ariadne Melo <ariadnevmelo@gmail.com>
2017-02-17 09:48:47 -0200

    ajuste na newsletter, refs #266

--- wp-content/themes/its-rio/footer.php
+++ wp-content/themes/its-rio/footer.php
@@ -146,18 +146,17 @@
 		},500);
 	}
 
-	function openNewsletter(){
+	function openNewsletter() {
 		$('.reveal-overlay').fadeIn();
 		$('#modalNewsletter iframe').attr('src','https://itsrio.us12.list-manage.com/subscribe/post?u=b2433258ec47fc6f9a063fd7b&id=8a308c4e7a&MERGE0='+$('#txtNewsletter').val());
-		setTimeout(function(){
-			if($('.reveal-overlay').is(':visible')){
-				$('html,body').css('position','relative');
-				$('html,body').css('overflow-y','hidden');
-			}else{
-				$('html,body').css('overflow-y','');
-				$('html,body').css('position','');
-			}
-		},500);
+		$('html,body').css('position','relative');
+		$('html,body').css('overflow-y','hidden');
+	}
+
+	function closeNewsletter() {
+		$('.reveal-overlay').fadeOut();
+		$('html,body').css('overflow-y','');
+		$('html,body').css('position','');
 	}
 
 	function changeMidia(obj, url, title){
