a873df8e0eef88e94dc13b5864763856da0965c0
brmagrini@gmail.com <brmagrini@gmail.com>
2020-04-08 01:40:25 -0300

    altera entrypoint para soluicionar copia do tema

--- compose/entrypoint-extra/sync-dependencies.sh
+++ compose/entrypoint-extra/sync-dependencies.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-DEV_MUPLUGINS="/var/www/mu-plugins"
-WP_MUPLUGINS="/var/www/html/wp-content/mu-plugins"
-
 DEV_PLUGINS="/var/www/plugins"
 WP_PLUGINS="/var/www/html/wp-content/plugins"
 
@@ -11,20 +8,7 @@ WP_THEMES="/var/www/html/wp-content/themes"
 
 UPLOADS_DIR="/var/www/html/wp-content/uploads"
 
-mkdir -p $WP_MUPLUGINS $WP_PLUGINS $WP_THEMES $UPLOADS_DIR && chown www-data: $WP_MUPLUGINS $WP_PLUGINS $WP_THEMES $UPLOADS_DIR
-
-if [ -d "$DEV_MUPLUGINS" ];
-then
-    find "$DEV_MUPLUGINS" \
-        -maxdepth 1 \
-        -mindepth 1 \
-        -exec cp -a {} $WP_MUPLUGINS/TMP \; \
-        -exec echo 'rm -rf '$WP_MUPLUGINS'/$(basename {})'  \; \
-        -exec sh -c 'rm -rf '$WP_MUPLUGINS'/$(basename {})'  \; \
-        -exec echo 'mv '$WP_MUPLUGINS'/TMP '$WP_MUPLUGINS'/$(basename {})' \; \
-        -exec sh -c 'mv '$WP_MUPLUGINS'/TMP '$WP_MUPLUGINS'/$(basename {})' \;
-fi
-
+mkdir -p $WP_PLUGINS $WP_THEMES $UPLOADS_DIR && chown www-data: $WP_PLUGINS $WP_THEMES $UPLOADS_DIR
 
 if [ -d "$DEV_PLUGINS" ];
 then
@@ -50,6 +34,6 @@ then
         -exec echo 'mv '$WP_THEMES'/TMP '$WP_THEMES'/$(basename {})' \; \
         -exec sh -c 'mv '$WP_THEMES'/TMP '$WP_THEMES'/$(basename {})' \;
 fi
-cp -ra plugins/* wp-content/plugins/
-cp -ra themes/* wp-content/themes/
+cp -ra /var/www/plugins/* wp-content/plugins/
+cp -ra /var/www/themes/* wp-content/themes/
 chown -R www-data:www-data /var/www/html/wp-content
