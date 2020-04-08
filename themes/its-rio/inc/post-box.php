7ec5c1197606985ca37b47348f782b84888870ef
brmagrini@gmail.com <brmagrini@gmail.com>
2020-04-08 00:03:22 -0300

    adiciona a classe dos cpts video e artigo no card do post

--- themes/its-rio/inc/post-box.php
+++ themes/its-rio/inc/post-box.php
@@ -1,11 +1,13 @@
 <?php  global $titles; $meta = get_post_meta(get_the_ID()); ?>
-<?php $class = ($post->post_type == 'projetos_ctp') ? 'area-'.$meta['info_areapesquisa'][0] : '' ?>
-<?php $class .= ($post->post_type == 'publicacoes_ctp' || $post->post_type == 'comunicados_ctp') ? $post->post_type : '' ?>
+<?php 
+$class = ($post->post_type == 'projetos_ctp') ? 'area-'.$meta['info_areapesquisa'][0] : '';
+$array_cpts = ['publicacoes_ctp','comunicados_ctp','videos_ctp', 'artigos_ctp' ];
+$class .= (in_array($post->post_type, $array_cpts)) ? $post->post_type : '' ?>
 <div class="list-item-wrapper column small-12 medium-4 large-4 end <?= $class; ?>">	<div <?php post_class( 'list-item' ); ?>>
 		<div class="info">
 			<p class="post-type">
 				<?php
-					$titlesCard = ['cursos_ctp' => 'cursos', 'varandas_ctp' => 'varandas', 'projetos_ctp' => 'projetos', 'publicacoes_ctp' => 'publicações', 'comunicados_ctp' => 'acontece'];
+					$titlesCard = ['cursos_ctp' => 'cursos', 'varandas_ctp' => 'varandas', 'projetos_ctp' => 'projetos', 'publicacoes_ctp' => 'publicações', 'comunicados_ctp' => 'acontece', 'videos_ctp' => 'vídeos', 'artigos_ctp' => 'artigos'];
 					
 					echo pll__($titlesCard[get_post_type()]);
 				?>
