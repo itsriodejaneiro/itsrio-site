de2597d8e0e21f994ddc5d9b88cd70e0bebe2ff4
willowm <brmagrini@gmail.com>
2020-04-08 23:07:20 -0300

    muda ordem do menu

--- themes/its-rio/header.php
+++ themes/its-rio/header.php
@@ -142,18 +142,18 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 									</li> 
 									<li id="menu-item-3617" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3617">
 										<a href="/<?= $lang ?>/projetos"><?= pll__('projetos') ?></a>
-									</li> 
-									<li id="menu-item-3622" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3622">
-										<a href="/<?= $lang ?>/cursos/in-company"><?= pll__('in company') ?></a>
-									</li>
-									<li id="menu-item-3621" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3621">
-										<a href="/<?= $lang == 'en' ? 'en/cursos/pos-graduacao' : 'pt/pos-graduacao' ?>/"><?= pll__('pós-graduação') ?></a>
-									</li>
+									</li> 							
 									<li id="menu-item-3619" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3619">
 										<a href="/<?= $lang == 'en' ? 'en/productions' : 'pt/producoes' ?>/"><?= pll__('produções') ?></a>
 									</li>
 									<li id="menu-item-3620" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3620">
 										<a href="/<?= $lang == 'en' ? 'en/institutional' : 'pt/institucional' ?>/"><?= pll__('institucional') ?></a>
+									</li>
+									<li id="menu-item-3622" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3622">
+										<a href="/<?= $lang ?>/cursos/in-company"><?= pll__('in company') ?></a>
+									</li>
+									<li id="menu-item-3621" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3621">
+										<a href="/<?= $lang == 'en' ? 'en/cursos/pos-graduacao' : 'pt/pos-graduacao' ?>/"><?= pll__('pós-graduação') ?></a>
 									</li>
 								</ul>
 							</div>
