45b75a979c32b76e1b49bb23dd3ab26431954862
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2021-11-23 13:09:57 -0300

    Create the healthcheck page - #15

---
+++ themes/its-rio/healthcheck.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="robots" content="noindex">
+    <title>Health Check</title>
+</head>
+<body>
+    <span style="display: none;">
+        <?php echo "Ok"; ?>
+    </span>
+</body>
+</html>
